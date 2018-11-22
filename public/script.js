175df0fd7a2810607f502cba7552c1dc0b9408e7
ColeSnyder <33235561+ColeSnyder@users.noreply.github.com>
2018-11-21 22:51:40 -0600

    Canvas Work
    Canvas clears now and the save functionality is almost set up.

--- public/script.js
+++ public/script.js
@@ -1,100 +1,52 @@
-window.onload = function(){
-    document.getElementById("myCanvas").style.width = "140px";
-    document.getElementById("myCanvas").style.height = "140px";
+function init() {
+    ctx = document.getElementById('myCanvas').getContext("2d");
 
-    // var canvas = document.getElementById("myCanvas");
-    // var ctx = canvas.getContext("2d");
+    $('#myCanvas').mousedown(function (e) {
+        mousePressed = true;
+        Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, false);
+    });
 
-    // ctx.beginPath();
-    // ctx.rect(20, 40, 50, 50);   
-    // ctx.fillStyle = "#FF0000";
-    // ctx.fill();
-    // ctx.closePath();
+    $('#myCanvas').mousemove(function (e) {
+        if (mousePressed) {
+            Draw(e.pageX - $(this).offset().left, e.pageY - $(this).offset().top, true);
+        }
+    });
+
+    $('#myCanvas').mouseup(function (e) {
+        mousePressed = false;
+    });
+	    $('#myCanvas').mouseleave(function (e) {
+        mousePressed = false;
+    });
 }
 
-var canvas, ctx, flag = false,
-        prevX = 0,
-        currX = 0,
-        prevY = 0,
-        currY = 0,
-        dot_flag = false;
-
-    var x = "black",
-        y = 2;
-    
-    function init() {
-        canvas = document.getElementById('myCanvas');
-        ctx = canvas.getContext("2d");
-        w = canvas.width;
-        h = canvas.height;
-    
-        canvas.addEventListener("mousemove", function (e) {
-            findxy('move', e)
-        }, false);
-        canvas.addEventListener("mousedown", function (e) {
-            findxy('down', e)
-        }, false);
-        canvas.addEventListener("mouseup", function (e) {
-            findxy('up', e)
-        }, false);
-        canvas.addEventListener("mouseout", function (e) {
-            findxy('out', e)
-        }, false);
-    }
-    
-    function draw() {
+function Draw(x, y, isDown) {
+    if (isDown) {
         ctx.beginPath();
-        ctx.moveTo(prevX, prevY);
-        ctx.lineTo(currX, currY);
-        ctx.strokeStyle = x;
-        ctx.lineWidth = y;
-        ctx.stroke();
+        ctx.strokeStyle = $('#selColor').val();
+        ctx.lineWidth = 4;
+        ctx.lineJoin = "round";
+        ctx.moveTo(lastX, lastY);
+        ctx.lineTo(x, y);
         ctx.closePath();
+        ctx.stroke();
     }
-    
-    function erase() {
-        var m = confirm("Want to clear");
-        if (m) {
-            ctx.clearRect(0, 0, w, h);
-            document.getElementById("canvasimg").style.display = "none";
-        }
-    }
-    
-    // function save() {
-    //     document.getElementById("canvasimg").style.border = "2px solid";
-    //     var dataURL = canvas.toDataURL();
-    //     document.getElementById("canvasimg").src = dataURL;
-    //     document.getElementById("canvasimg").style.display = "inline";
-    // }
-    
-    function findxy(res, e) {
-        if (res == 'down') {
-            prevX = currX;
-            prevY = currY;
-            currX = e.clientX - canvas.offsetLeft;
-            currY = e.clientY - canvas.offsetTop;
-    
-            flag = true;
-            dot_flag = true;
-            if (dot_flag) {
-                ctx.beginPath();
-                ctx.fillStyle = x;
-                ctx.fillRect(currX, currY, 2, 2);
-                ctx.closePath();
-                dot_flag = false;
-            }
-        }
-        if (res == 'up' || res == "out") {
-            flag = false;
-        }
-        if (res == 'move') {
-            if (flag) {
-                prevX = currX;
-                prevY = currY;
-                currX = e.clientX - canvas.offsetLeft;
-                currY = e.clientY - canvas.offsetTop;
-                draw();
-            }
-        }
-    }
-    
+    lastX = x; lastY = y;
+}
+
+function clearArea() {
+    // Use the identity matrix while clearing the canvas
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+}
+
+function Save() {
+//   var canvas1 = document.getElementById("myCanvas");        
+//   if (canvas1.getContext) {
+//     alert("Into Loop");
+//     var image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+//     window.location.href=image;    
+//   }
+//   var imageElement = document.getElementById("MyPix");  
+//   imageElement.src = myImage;
+}

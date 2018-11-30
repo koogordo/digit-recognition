7f605a5176346abb12f6d0610e92dbd344cfb6b4
CredieNogueiraDeLima <uh6456jv@uh6456jvm7.workstations.winona.edu>
2018-11-29 19:57:08 -0600

    changed save method

--- public/canvas.js
+++ public/canvas.js
@@ -2,7 +2,7 @@ let data = [];
 
 function setup() {
   let size = 280;
-  pixelDensity(0.05);
+  // pixelDensity(0.05);
   var myCanvas = createCanvas(size, size);
   myCanvas.parent('input');
   background(255);
@@ -16,23 +16,35 @@ function touchMoved() {
 }
 
 function savePixels() {
-  loadPixels();
-  let compressed_pixels = pixels.map(pix => {
-    return pix / 255;
-  });
+  let img = get(); //makes canvas into p5 img object
+  let input = [];
+  img.resize(28,28);
+  img.loadPixels();
+  for (let i = 0; i < img.pixels.length; i+= 4) {
+     let pix = img.pixels[i];
+      input[i/4] = 1 - (pix/255);
+    
+  }
 
-  console.log(compressed_pixels.length);
-  console.table(compressed_pixels);
-  let rawPayload = { data: compressed_pixels };
-  let payload = JSON.stringify(rawPayload);
-  let url = 'http://localhost:3000/tensor/predict';
-  $.ajax({
-    type: 'POST',
-    url: url,
-    contentType: 'application/json',
-    data: payload,
-    success: console.log('success')
-  });
+  console.log(input);
+  // loadPixels();
+
+  // let compressed_pixels = pixels.map(pix => {
+  //   return pix / 255;
+  // });
+
+  // console.log(compressed_pixels.length);
+  // console.log(img);
+  // let rawPayload = { data: compressed_pixels };
+  // let payload = JSON.stringify(rawPayload);
+  // let url = 'http://localhost:3000/tensor/predict';
+  // $.ajax({
+  //   type: 'POST',
+  //   url: url,
+  //   contentType: 'application/json',
+  //   data: payload,
+  //   success: console.log('success')
+  // });
 }
 
 $('#predict').click(function() {
